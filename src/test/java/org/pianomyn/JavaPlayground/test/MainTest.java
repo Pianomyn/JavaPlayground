f8a995bacd802537b8d0a70fa2337ac09afc26f8
Pianomyn <anditan@skiff.com>
2024-04-28 19:30:46 +1000

    autoformat

--- src/test/java/org/pianomyn/JavaPlayground/test/MainTest.java
+++ src/test/java/org/pianomyn/JavaPlayground/test/MainTest.java
@@ -3,12 +3,11 @@ package org.pianomyn.JavaPlayground.test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
-import org.pianomyn.JavaPlayground.Main;
 
 public class MainTest {
   @Test
   public void simpleAddTest() {
-    assert (1+2 == 3);
-    assertFalse(1+2 == 100);
+    assert (1 + 2 == 3);
+    assertFalse(1 + 2 == 100);
   }
 }
