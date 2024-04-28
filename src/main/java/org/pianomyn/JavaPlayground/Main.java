ac2d873e53cff8f366dcf94c42c5292c07621e88
Pianomyn <anditan@skiff.com>
2024-04-28 19:34:27 +1000

    helloworld

--- src/main/java/org/pianomyn/JavaPlayground/Main.java
+++ src/main/java/org/pianomyn/JavaPlayground/Main.java
@@ -1,5 +1,7 @@
 package org.pianomyn.JavaPlayground;
 
 public class Main {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+  }
 }
